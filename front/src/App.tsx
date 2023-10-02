e0f70c11ec9c0782e2a7fb62660857e8c3bbac7e
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-02 13:41:27 +0400

    tailwind installed

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import type { JSXElement } from 'solid-js';
 
 export default function (): JSXElement {
-  return (<div>Yo world</div>);
+  return (<div class="text-3xl font-bold underline">Yo world</div>);
 };
