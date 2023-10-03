f38b253bf2e13c9c0a2d94c9136f1100c6ce1a14
Quentin PAYET <quentinpayet1203@gmail.com>
2023-10-03 13:50:29 +0400

    wip: menu button

--- front/src/components/menu.tsx
+++ front/src/components/menu.tsx
@@ -1,13 +1,18 @@
 import { gameLanguage } from "../App";
 import { gameModeIA, gameModeLocal, gameModeOnline } from "./languageDict";
 
+// TODO: Créer un composant MenuButton
+// TODO: Mettre en place un mouseOver shadow sur les boutons
+
 export default function () {
     return (
-        <div class="flex justify-center">
+        <div class="grid grid-cols-3 justify-center mt-10 h-40">
             {/* <div class="grid grid-cols-3"> */}
-                <div>{gameModeLocal[gameLanguage]}</div>
-                <div class="px-20">{gameModeOnline[gameLanguage]}</div>
-                <div>{gameModeIA[gameLanguage]}</div>
+                <div class="text-center h-fit">{gameModeLocal[gameLanguage]}</div>
+                <div class="text-center mx-20 h-fit">{gameModeOnline[gameLanguage]}</div>
+                <div class="mx-5">
+                    <div class="text-center h-fit">{gameModeIA[gameLanguage]}</div>
+                </div>
             {/* </div> */}
         </div>
     )
